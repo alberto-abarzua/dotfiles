bf057e4d139920fdca39d2ca54ac756a3e56ca00
alberto-abarzua <uintuser@pm.me>
2023-10-12 11:13:22 -0300

    chore: update general nvim settings

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -5,6 +5,18 @@ vim.g.loaded_netrw = 1
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+-- Indentation
+vim.o.smartindent = true
+vim.o.autoindent = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
+
+-- Cursor settings
+vim.o.scrolloff = 8
+
+-- Clipboard
+vim.o.clipboard = "unnamedplus"
+
+-- General
+vim.o.autoread = true
