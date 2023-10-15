53d39ec45d24c34db8970a014434f572e587da18
alberto-abarzua <uintuser@pm.me>
2023-10-15 13:15:56 -0300

    fix: update install script

--- install.py
+++ install.py
@@ -24,21 +24,31 @@ TO_HOME = [
 if __name__ == '__main__':
     print("Installing dotfiles...")
 
+    if USER_HOME is None:
+        print("HOME is not set, exiting...")
+        exit(1)
     if USER_XDG_CONFIG_HOME is None:
         print("XDG_CONFIG_HOME is not set, using HOME/.config")
+        USER_XDG_CONFIG_HOME = USER_HOME + "/.config"
+
+    USER_HOME_PATH = Path(USER_HOME)
+    USER_XDG_CONFIG_HOME_PATH = Path(USER_XDG_CONFIG_HOME)
+
     failed_files = []
 
     for path in TO_CONFIG_HOME:
         # os.system(f"ln -s {path} {USER_XDG_CONFIG_HOME}")
         exit_code = os.system(f"ln -s {path} {USER_XDG_CONFIG_HOME}")
         if exit_code != 0:
-            failed_files.append(path)
+            file_to_rm = USER_XDG_CONFIG_HOME_PATH / path.name
+            failed_files.append(file_to_rm)
 
     for path in TO_HOME:
         for file in path.iterdir():
             exit_code = os.system(f"ln -s {file} {USER_HOME}")
             if exit_code != 0:
-                failed_files.append(file)
+                file_to_rm = USER_HOME_PATH / file.name
+                failed_files.append(file_to_rm)
 
     print("Some files failed to install:")
     for file in failed_files:
