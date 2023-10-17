07baba0d7d5eab8cd9ef60c7f706eb6d2d95f416
alberto-abarzua <uintuser@pm.me>
2023-10-16 21:18:10 -0300

    fix: scrolloff not working with tmux nav plugin

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -76,11 +76,10 @@ cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
 		["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-		["<C-y>"] = cmp.mapping.confirm({ select = true }),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<Tab>"] = nil,
 		["<S-Tab>"] = nil,
 	}),
 })
-
 vim.lsp.set_log_level("trace")
