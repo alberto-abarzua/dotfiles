fd340007e0d14e276e67a66b9d485159a4bb8dc3
alberto-abarzua <uintuser@pm.me>
2023-11-05 18:52:24 -0300

    chore :udate zshrc

--- nvim/lua/plugins/configs/ale.lua
+++ nvim/lua/plugins/configs/ale.lua
@@ -40,15 +40,15 @@ vim.g.ale_fixers = {
 vim.g.ale_python_autopep8_options = "--max-line-length 120"
 -- Make ale use prettier global for formatting
 
-vim.g.ale_javascript_prettier_executable = "prettier"
-vim.g.ale_typescript_prettier_executable = "prettier"
-vim.g.ale_typescriptreact_prettier_executable = "prettier"
-vim.g.ale_javascriptreact_prettier_executable = "prettier"
-
-vim.g.ale_javascript_eslint_executable = "eslint"
-vim.g.ale_tyescript_eslint_executable = "eslint"
-vim.g.ale_tyescriptreact_eslint_executable = "eslint"
-vim.g.ale_javascriptreact_eslint_executable = "eslint"
+-- vim.g.ale_javascript_prettier_executable = "prettier"
+-- vim.g.ale_typescript_prettier_executable = "prettier"
+-- vim.g.ale_typescriptreact_prettier_executable = "prettier"
+-- vim.g.ale_javascriptreact_prettier_executable = "prettier"
+--
+-- vim.g.ale_javascript_eslint_executable = "eslint"
+-- vim.g.ale_tyescript_eslint_executable = "eslint"
+-- vim.g.ale_tyescriptreact_eslint_executable = "eslint"
+-- vim.g.ale_javascriptreact_eslint_executable = "eslint"
 
 -- Optional: Use ALE with specific file types
 vim.g.ale_lint_on_text_changed = "never" -- Disable linting as you type
