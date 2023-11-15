b51b22b74f2f4d8631f2c43003975520d6c8f58f
alberto-abarzua <uintuser@pm.me>
2023-11-15 16:19:13 -0300

    chore: remove telescope unused mapping

--- nvim/lua/plugins/configs/telescope.lua
+++ nvim/lua/plugins/configs/telescope.lua
@@ -10,6 +10,7 @@ vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set("n", "<leader>gt", function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end, {})
+
 -- Map <leader>vh to help_tags
 vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
 
@@ -21,26 +22,3 @@ vim.api.nvim_set_keymap(
 	{ noremap = true, silent = true }
 )
 
--- Map to a key sequence
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>ffd",
-	[[<Cmd>lua find_file_and_function()<CR>]],
-	{ noremap = true, silent = true }
-)
-
--- Function to find file and then function within that file
-function find_file_and_function()
-	require("telescope.builtin").find_files({
-		attach_mappings = function(_, map)
-			map("i", "<CR>", function(prompt_bufnr)
-				local selection = require("telescope.actions.state").get_selected_entry(prompt_bufnr)
-				require("telescope.actions").close(prompt_bufnr)
-				local filename = selection.value
-				vim.cmd("edit " .. filename)
-				require("telescope.builtin").lsp_document_symbols()
-			end)
-			return true
-		end,
-	})
-end
